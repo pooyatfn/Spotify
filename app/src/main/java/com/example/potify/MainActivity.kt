713d159d17469de3a6a68e48afdaed589c409a8c
pooya <pooyatoofan@aut.ac.ir>
2023-05-07 15:38:44 +0330

    Initial commit

---
+++ app/src/main/java/com/example/potify/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.potify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+}
