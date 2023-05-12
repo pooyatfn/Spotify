59dfda6e8e664f6c5a3ebc1a7d9a262557e1a1f8
pooya <pooyatoofan@aut.ac.ir>
2023-05-12 20:06:56 +0330

    edit bottom navigation

--- app/src/main/java/com/example/potify/MainActivity.kt
+++ app/src/main/java/com/example/potify/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.potify
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build.VERSION_CODES.R
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.potify.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding = DataBindingUtil.inflate(LayoutInflater.from(this), R.layout.activity_main, null, false)
+
+        val navController = findNavController(this, R.id.myNavHostFragment)
+        //NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
---
+++ app/src/main/java/com/example/potify/SearchFragment.kt
@@ -0,0 +1,18 @@
+package com.example.potify
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class SearchFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search, container, false)
+    }
+
+}
